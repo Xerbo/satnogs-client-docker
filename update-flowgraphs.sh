fe83fa037ead228a3b6b2aeb9c60f7db1802d45c
Xerbo <xerbo@protonmail.com>
2022-08-30 20:03:01 +0100

    Fix update-flowgraphs.sh

--- update-flowgraphs.sh
+++ update-flowgraphs.sh
@@ -1,1 +1,7 @@
-docker exec satnogs-node$1 bash -c "cd /usr/bin/flowgraphs && for i in *.grc; do; grcc \"$i\"; done; chown satnogs:satnogs *"
+#!/usr/bin/env bash
+if [ -z "$1" ]; then
+    echo "Usage: $0 STATION_NAME"
+    exit
+fi
+
+docker exec "satnogs-$1" bash -c "cd /usr/bin/flowgraphs && for i in *.grc; do; grcc \"$i\"; done; chown satnogs:satnogs *"
